369a06e40fc25c907cd9394ab217e72dbf9bcee6
DannyWeeks <DannyWeeks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-06 15:54:33 +0000

    Version 1.3.1.1
    git-svn-id: https://plugins.svn.wordpress.org/simple-custom-website-data@852673 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.3.1.1/views/utility.php
@@ -0,0 +1,10 @@
+<h3 class="page-title">Utility</h3>
+<ul>
+    <li><strong>Backup</strong> all records by exporting them - <a class="actionbtn" href="<?php echo site_url();?>/wp-admin/admin.php?page=cwd-management&export=true">Export</a></li>
+    <li><strong>Import</strong> CSV of records - <a class="actionbtn" href="<?php echo site_url();?>/wp-admin/admin.php?page=cwd-management&view=import">import</a></li>
+</ul>
+<br>
+<br>
+<hr>
+<br>
+<a class="actionbtn" href="<?php echo site_url();?>/wp-admin/admin.php?page=cwd-management">Dashboard</a>
