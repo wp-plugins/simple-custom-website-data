02ec43d67400dc6ca5e0482166d5cfac400608d8
DannyWeeks <DannyWeeks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-07 17:33:27 +0000

    Version 2.0 added classes
    git-svn-id: https://plugins.svn.wordpress.org/simple-custom-website-data/trunk@1021632 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/Messages.php
@@ -0,0 +1,35 @@
+<?php namespace Cwd;
+
+class Messages {
+
+    public function showMessage($message, $errormsg = false)
+    {
+        if ($errormsg)
+        {
+            echo '<div id="message" class="error">';
+        }
+        else
+        {
+            echo '<div id="message" class="updated fade">';
+        }
+
+        echo '<p><strong>' . $message . '</strong></p></div>';
+    }
+
+    public function showAdminMessages()
+    {
+        if ($_SESSION['cwd_message'])
+        {
+            $this->showMessage($_SESSION['cwd_message']);
+            $_SESSION['cwd_message'] = false;
+        }
+    }
+
+    public function setMessage($message = null)
+    {
+        if ($_SESSION['cwd_started'])
+        {
+            $_SESSION['cwd_message'] = $message;
+        }
+    }
+}
---
+++ views/whats-new.php
@@ -0,0 +1,25 @@
+<h3 class="page-title">What's new in version <?php echo CWD_VERSION ?></h3>
+
+<ul class="ul-list">
+    <li>
+        Out with the 90's style; in with the modern Wordpress look.
+    </li>
+    <li>
+        Added sub menu.
+    </li>
+    <li>
+        Arrays are displayed in a much cleaner fashion on the dashboard.
+    </li>
+    <li>
+        Entire code base refactored to be cleaner, more readable and more efficient.
+    </li>
+    <li>
+        Automatically select the shortcode from the dashboard by clicking it.
+    </li>
+</ul>
+
+<br>
+<br>
+<hr>
+<br>
+<a class="actionbtn" href="<?php echo site_url();?>/wp-admin/admin.php?page=cwd-management">Dashboard</a>
